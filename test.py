9097b8ee859831492e20a1844fa358bad5d407b1
Xingyu <antaresshao@outlook.com>
2024-08-30 09:58:39 +0000

    已经加上了可适应的预置，经过debug已经能跑通。
    Changes to be committed:
    	modified:   test.py

--- test.py
+++ test.py
@@ -8,13 +8,13 @@ from classifiers import LinearLayer
 import parser
 args = parser.parse_arguments()
 
+from generate_database import M_list, flight_heights
+
 
 LR_N = [1, 5, 10, 20]
 # threshold = 25  # ORIGION 原来的M=20m，这里设置的是偏移25米算成功召回
 # threshold = 500 # EDIT 这里是要配合M的设定，M设为800米
 threshold = args.threshold  # EDIT
-# if threshold is None:
-#     threshold = 
 
 def compute_pred(criterion, descriptors):
     if isinstance(criterion, LinearLayer):
@@ -31,6 +31,9 @@ def inference(args, model, classifiers, test_dl, groups, num_test_images):
     model = model.eval()
     classifiers = [c.to(args.device) for c in classifiers]
     valid_distances = torch.zeros(num_test_images, max(LR_N))
+
+    # EDIT 加上可变threshold的初始化
+    threshold_list_total = torch.zeros(num_test_images, max(LR_N))
         
     all_preds_utm_centers = [center for group in groups for center in group.class_centers]
     all_preds_utm_centers = torch.tensor(all_preds_utm_centers).to(args.device)
@@ -50,15 +53,37 @@ def inference(args, model, classifiers, test_dl, groups, num_test_images):
                 
             topn_pred_class_id = all_preds_confidences.argsort(descending=True)[:max(LR_N)]
             pred_class_id = all_preds_utm_centers[topn_pred_class_id]    # NOTE 这里能不能把从classid里得到自适应M然后得到threshold
-            dist=torch.cdist(query_utms.unsqueeze(0), pred_class_id.to(torch.float64))
+            
+            # ORIGION
+            # dist=torch.cdist(query_utms.unsqueeze(0), pred_class_id.to(torch.float64))
+            # valid_distances[query_i] = dist
+
+            # EDIT
+            if threshold is None:
+                Heights = list(map(int, pred_class_id[:,0].tolist())) # 将list中的数值全部变为int类型，作为索引
+                M_indexes = [flight_heights.index(h) for h in Heights]
+                M_params = [M_list[idx] for idx in M_indexes]
+                threshold_list = M_params
+            else:
+                threshold_list = [threshold] * max(LR_N)    # 所有元素都相同
+
+            dist = torch.cdist(query_utms.unsqueeze(0), pred_class_id.to(torch.float64))
             valid_distances[query_i] = dist
-                
+            threshold_list_total[query_i] = torch.tensor(threshold_list)
+
     classifiers = [c.cpu() for c in classifiers]
     torch.cuda.empty_cache()  # Release classifiers memory
     lr_ns = []
     for N in LR_N:
-        # lr_ns.append(torch.count_nonzero((valid_distances[:, :N] <= 25).any(axis=1)).item() * 100 / num_test_images)    # ORIGION
-        lr_ns.append(torch.count_nonzero((valid_distances[:, :N] <= threshold).any(axis=1)).item() * 100 / num_test_images) # EDIT 把判定阈值给改了
+        # lr_ns.append(torch.count_nonzero((valid_distances[:, :N] <= 25).any(axis=1)).item() * 100 / num_test_images)    # ORIGION  计算20个cell有多少成功匹配的
+        # lr_ns.append(torch.count_nonzero((valid_distances[:, :N] <= threshold).any(axis=1)).item() * 100 / num_test_images) # EDIT 把判定阈值给改了
+
+        # EDIT
+        valid_list = []
+        for img_idx in range(num_test_images):
+            valid_topn = [valid_distances[img_idx, n] <= threshold_list_total[img_idx, n] for n in range(N)]
+            valid_list.append(valid_topn) # EDIT 改成可以自适应的阈值
+        lr_ns.append(torch.count_nonzero(torch.tensor(valid_list).any(axis=1)).item() * 100 / num_test_images)
 
     gcd_str = ", ".join([f'LR@{N}: {acc:.1f}' for N, acc in zip(LR_N, lr_ns)])
     
